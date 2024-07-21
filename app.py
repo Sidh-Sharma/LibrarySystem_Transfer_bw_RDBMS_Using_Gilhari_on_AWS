a781ceb9798f99b1c2c7936a8deecce4e03e2ccf
Siddharth Sharma <f20220601@pilani.bits-pilani.ac.in>
2024-07-21 21:18:16 +0530

    update app.py

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def end_application():
     return jsonify({"message": "Application is closed"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port='80',debug=True)
